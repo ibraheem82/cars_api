e0645e21f2ab29cfc78110c36baa64dd1f6491ad
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-08-01 05:06:33 +0100

    installed python decouple [pip install python-decouple]

--- cars/cars/settings.py
+++ cars/cars/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k5(n9w#w1f&n36$(v5ulr_08_41c%y3^$uqv@@df4=#xdcn6!3'
+SECRET_KEY =  
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
