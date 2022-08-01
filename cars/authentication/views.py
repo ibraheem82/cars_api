17c9b57c8b0c0197f477b164a6eacb1cdb7e50ac
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-08-01 05:38:52 +0100

    i imported the [generics, status And Response] in order to test the first {Api} message

--- cars/authentication/views.py
+++ cars/authentication/views.py
@@ -1,3 +1,10 @@
+from re import I
 from django.shortcuts import render
-
+from rest_framework import generics, status
+from rest_framework.response import Response 
 # Create your views here.
+
+class HelloAuthView(generics.GenericAPIView):
+    def get(self, request):
+        return Response(data={"Message": "Hello Auth"}, status=status.HTTP_200_OK)
+        
