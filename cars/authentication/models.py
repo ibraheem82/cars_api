85e3fbb11c84fb0d26a6a8e0c0bc06c8a48622cd
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-08-02 10:36:37 +0100

    imported BaseUserManager -> [django.contrib.auth.base_user]

--- cars/authentication/models.py
+++ cars/authentication/models.py
@@ -2,3 +2,7 @@ from django.db import models
 
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+
+
+# class CustomUserManager()
