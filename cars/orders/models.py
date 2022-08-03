37716b621f00a977b02dc09de89351b91dbb2897
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-08-03 09:44:32 +0100

    created [Order] model, and assign Customer to the [User] model using the ForeignKey

--- cars/orders/models.py
+++ cars/orders/models.py
@@ -1,3 +1,36 @@
+from ctypes import sizeof
 from django.db import models
+# * [get_user_model] helps us to get our user model even without calling our user model.
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
+class Order(models.Model):
+    
+    BRANDS = (
+        ('TOYOTA', 'toyota'),
+        ('CHEVROLET', 'chevrolet'),
+        ('HONDA', 'honda'),
+        ('FORD', 'ford'),
+        ('FIAT', 'fiat'),
+        ('JEEP', 'jeep'),
+        ('VOLKSWAGEN', 'volkswagen'),
+        ('VOLVO', 'volvo'),
+        ('AUDI', 'audi'),
+        ('LAND_ROVER', 'landrover'),
+        ('LEXUS', 'lexus'),
+        ('PORSHE', 'porshe'),
+        ('BUGATTI', 'bugatti'),
+    )
+    
+    ORDER_STATUS = (
+        'PENDING', 'pending',
+        'IN_TRANSIT', 'intransit',
+        'DELIVERED', 'delivered',
+    )
+    
+    customer           = models.ForeignKey(User, on_delete=models.CASCADE)
+    size               = models.CharField(max_length = 20, choices=SIZES)
+    # cars_description = 
