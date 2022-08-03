514893b078298ea370afb2accf8d5ae5ac83bb94
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-08-03 12:00:29 +0100

    registered the [Order] admin use the {@admin.register} decorator to customize the [list_display] which is what you want to dispaly.

--- cars/orders/admin.py
+++ cars/orders/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Order
 # Register your models here.
+# * Register this for customization
+@admin.register(Order)
+# * Everytime we want to create our custom 
+
+class OrderAdmin(admin.ModelAdmin):
+    # * list of things you want to display.
+    list_display = ['brand', 'order_status', 'quantity']
