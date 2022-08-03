810371107e1bc1ee9879f3ba2728ab9222c83f0a
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-08-03 11:48:41 +0100

    changed the from [sizes] to brand for the model.

--- cars/orders/models.py
+++ cars/orders/models.py
@@ -1,4 +1,4 @@
-from ctypes import sizeof
+
 from django.db import models
 # * [get_user_model] helps us to get our user model even without calling our user model.
 from django.contrib.auth import get_user_model
@@ -26,11 +26,22 @@ class Order(models.Model):
     )
     
     ORDER_STATUS = (
-        'PENDING', 'pending',
-        'IN_TRANSIT', 'intransit',
-        'DELIVERED', 'delivered',
+        ('PENDING', 'pending'),
+        ('IN_TRANSIT', 'intransit'),
+        ('DELIVERED', 'delivered'),
     )
     
     customer           = models.ForeignKey(User, on_delete=models.CASCADE)
-    size               = models.CharField(max_length = 20, choices=SIZES)
-    # cars_description = 
+    brand              = models.CharField(max_length = 20, choices=BRANDS, default=BRANDS[0][0])
+    order_status       = models.CharField(max_length=20, choices=ORDER_STATUS, default=ORDER_STATUS[0][0])
+    quantity           = models.IntegerField()
+    cars_description   = models.TextField(null=True, blank=True)
+    cars_images        = models.ImageField()
+    created_at         = models.DateTimeField(auto_now_add=True)
+    updated_at         = models.DateTimeField(auto_now = True)
+
+
+
+
+    def __str__(self):
+        return f"<Order {self.brand} by {self.customer.id}"
