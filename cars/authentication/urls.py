ebce82bebf8d703380802a2d3a03c1b9af479f7f
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-08-14 10:05:27 +0100

    Added the [signup) route.

--- cars/authentication/urls.py
+++ cars/authentication/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.WelcomeAuthView.as_view(), name='welcome_auth'),
+    path('signup/', views.UserCreateView.as_view(), name='sign_up'),
 ]
